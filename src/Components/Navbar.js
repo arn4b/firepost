66c00d4496041b65d0afc575cb498a5eb146206a
Arnab Chatterjee <arnabc1701@gmail.com>
2021-03-10 22:07:54 +0530

    navbar designed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,9 +4,9 @@ class Navbar extends Component{
     render(){
         return(
             <div class="topnav">
-            <a class="active" href="#home">Home</a>
-            <button>About</button>
-            <button>Login</button>
+                <a class="active" href="#home">Home</a>
+                <button>About</button>
+                <button>Login</button>
             </div>
         );
     }
