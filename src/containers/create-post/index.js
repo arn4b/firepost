15b915a6af97e0caea9e8aa47c888d924f292a0f
Arnab Chatterjee <arnabc1701@gmail.com>
2021-03-25 13:13:23 +0530

    create post skeleton added

--- src/containers/create-post/index.js
+++ src/containers/create-post/index.js
@@ -1,12 +1,18 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { SignInBtn } from '../../components'
 import { UserContext } from '../../contexts/user';
 import "./style.css";
 
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+
 export default function CreatePost() {
 
     const [user, setuser] = useContext(UserContext).user;
+    const [caption, setCaption] = useState("");
 
+    const handleChange = () => {
+
+    }
     return (
         <div>
 
@@ -15,7 +21,25 @@ export default function CreatePost() {
 
             ?
 
-            <p>Hello {user.displayName}</p>
+            <div className="cp_main">
+                <p>Create Post</p>
+
+                <div className="cp_center">
+                    <textarea className="cp_text" rows="3" value = {caption} onChange={(e) => setCaption(e.target.value) }>
+
+                    </textarea>
+                </div>
+
+                <div className = "cp_img">
+                    
+                    <label htmlFor = "fileInput">
+                        <AddAPhotoIcon style = {{cursor: "pointer", fontSize: "50px"}}/>
+                    </label>
+                    <input id="fileInput" type="file" accept="image/*" onChange={handleChange}>
+
+                    </input>
+                </div>
+            </div>
 
             :
 
